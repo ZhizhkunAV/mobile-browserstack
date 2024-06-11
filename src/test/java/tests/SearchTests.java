5f3d542852b9f6bd1b656b2e0419072e90911208
Andrey <zizkun.av@list.ru>
2024-06-11 17:42:51 +0400

    Commit

--- src/test/java/tests/SearchTests.java
+++ src/test/java/tests/SearchTests.java
@@ -4,6 +4,8 @@ import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.CollectionCondition.sizeGreaterThan;
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.$$;
 import static io.appium.java_client.AppiumBy.accessibilityId;
@@ -13,26 +15,25 @@ import static io.qameta.allure.Allure.step;
 public class SearchTests extends TestBase {
 
     @Test
-    @Tag("ios")
-    void successfulSearchTest() {
+    @Tag("android")
+    void androidSuccessfulSearchTest() {
         step("Type search", () -> {
             $(accessibilityId("Search Wikipedia")).click();
-            $(id("org.wikipedia.alpha:id/search_src_text")).sendKeys("Appium");
+            $(id("org.wikipedia.alpha:id/search_src_text")).sendKeys("java");
         });
         step("Verify content found", () ->
                 $$(id("org.wikipedia.alpha:id/page_list_item_title"))
                         .shouldHave(sizeGreaterThan(0)));
     }
 
-    @Test
-    @Tag("android")
-    void successfulSearchAnotherTest() {
+    @Test()
+    @Tag("ios")
+    void iosCheckInputTest() {
         step("Type search", () -> {
-            $(accessibilityId("Search Wikipedia")).click();
-            $(id("org.wikipedia.alpha:id/search_src_text")).sendKeys("Henry VIII");
+            $(accessibilityId("Text Button")).click();
         });
-        step("Verify content found", () ->
-                $$(id("org.wikipedia.alpha:id/page_list_item_title"))
-                        .shouldHave(sizeGreaterThan(0)));
+        step("Check that text input should be visible ", () -> {
+            $(accessibilityId("Text Input")).shouldBe(visible);
+        });
     }
 }
